bdb8d836e68f0189f187a76097fd315ac6e147ac
Luke Granger <grangerl330@gmail.com>
2019-01-11 03:05:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  pigeon_names = data.collect do |attributes, details|
+    details.values
+  end 
+  
+  pigeon_names = pigeon_names.flatten.uniq
+  
+  # First I create an array, pigeon_names, that contains all of the pigeon_names from data. I use .flatten.uniq to first form a single array, then remove any duplicate elements. The array now contains 1 of each name 
+  
+  pigeon_names.each do |name|
+    pigeon_list[name] = {}
+  end 
+  
+  #This creates keys in the pigeon_list hash that are each of the names from the pigeon_names array 
+  
+  data.each do |attributes, detais|
+    pigeon_list.each do |name, hash|
+      pigeon_list[name][attributes] = []
+    end
+  end
+  
+  #Iterates through data yielding attributes and details, then iterates through the pigeon_list we created, yielding names and the empty hashes, then adds attributes as a key with values of an empty array within each name key that 
+  
+  
+  binding.pry 
 end
